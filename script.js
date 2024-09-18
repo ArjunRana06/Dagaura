c4bb16ee5da6385a27dd332b6cce0a22810c7cdc
Dinesh <dineshchy1231@gmail.com>
2024-09-18 15:06:30 +0545

    Added the addition file

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let c = 12;
-let d = 13;
-let sum = c + d;
+// Variables for arithmetic operations
+let num1 = 10;
+let num2 = 5;
+
+// Addition
+let sum = num1 + num2;
+console.log("Addition: " + sum); // Output: 15
+
+// Subtraction
+let difference = num1 - num2;
+console.log("Subtraction: " + difference); // Output: 5
+
+// Multiplication
+let product = num1 * num2;
+console.log("Multiplication: " + product); // Output: 50
+
+// Division
+let quotient = num1 / num2;
+console.log("Division: " + quotient); // Output: 2
